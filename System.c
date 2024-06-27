8562883f27083ea2d6c38a469b8bf84c0af57842
blacky <lucasivanestrada1@gmail.com>
2024-06-27 00:14:28 -0300

    update

--- System.c
+++ System.c
@@ -393,13 +393,11 @@ void menuLibrary()
             break;
 
         case 50:
-
-//                void showLibraryAtoZ(stSong[]);
+            showLibraryAtoZ();
             break;
 
         case 51:
-
-//                void showLibraryGenre(stSong[]);
+            showLibraryGenre();
             break;
         case 52:
 
